b90e41bf6550f90b47e90bcbb184ae9e8dfb50cd
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-08 11:17:33 +0530

    Completed Routes

--- Routes/product.routes.js
+++ Routes/product.routes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = require('router').Router();
-const getAllProducts = require('../Controllers/product.controller');
+const productControllers = require('../Controllers/product.controller');
 
 // Create a product
-router.post('/api/products', )
+router.post('/api/products', productControllers.createProduct);
 
 // Get all products
-router.get('/api/products', )
+router.get('/api/products', productControllers.getAllProducts);
 
 // Get a product by product id
-router.get('/api/products/:id', )
+router.get('/api/products/:id', productControllers.getProductById);
 
 // Update Product Details
-router.put('/api/products/:id', )
+router.put('/api/products/:id', productControllers.updateProductById);
 
 // Delete a product by id
-router.delete('/api/products/:id',)
+router.delete('/api/products/:id', productControllers.deletedProductById);
