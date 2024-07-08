91b842844f8f09750e533fc10d64ec6d78c099d0
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-08 09:45:55 +0530

    GET and PUT Methods

--- index.js
+++ index.js
@@ -11,73 +11,66 @@ mongoose.connect("mongodb+srv://abhishekshah5486:DJcl8vvHR43BRlQ5@cluster0.j9ind
 
 // Middleware function (converts string to json format)
 app.use(express.json());
-app.use(middleware);
-app.use(requestLogger);
+// app.use(middleware);
+// app.use(requestLogger);
 
-let courses = [
-    {id : 1, name : 'java'},
-    {id : 2, name : 'javascript'},
-    {id : 3, name : 'python'}
-];6
-app.get('/courses', (req, res) => {
-    res.json(courses);
-});
+// let courses = [
+//     {id : 1, name : 'java'},
+//     {id : 2, name : 'javascript'},
+//     {id : 3, name : 'python'}
+// ];
+// app.get('/courses', (req, res) => {
+//     res.json(courses);
+// });
 
-app.post('/courses', (req, res) => {
-   const name = req.body.name;
-   const newCourse = {id : courses.length+1, name : name};
-   courses.push(newCourse);
-   res.send(courses);
-})
+// app.post('/courses', (req, res) => {
+//    const name = req.body.name;
+//    const newCourse = {id : courses.length+1, name : name};
+//    courses.push(newCourse);
+//    res.send(courses);
+// })
 
 // Update id 1, name : name --> spring
 // Delete id 2 course;
-app.put('/courses', (req, res) => {
-    try {
-        const id = req.body.id;
-        const name = req.body.name;
-        for (let j=0; j<courses.length; j++){
-            if (courses[j].id === id){
-                courses[j].name = name;
-            }
-        }
-        res.send(courses);
-    } catch (err) {
-        res.status(505).send(err);
-    }
-})
+// app.put('/courses', (req, res) => {
+//     try {
+//         const id = req.body.id;
+//         const name = req.body.name;
+//         for (let j=0; j<courses.length; j++){
+//             if (courses[j].id === id){
+//                 courses[j].name = name;
+//             }
+//         }
+//         res.send(courses);
+//     } catch (err) {
+//         res.status(505).send(err);
+//     }
+// })
 
-app.delete('/courses', (req, res) => {
-    const id = req.body.id;
-    for (let j=0; j<courses.length; j++){
-        if (courses[j].id === id){
-            courses.splice(j, 1);
-        }
-    }
-    res.send(courses);
-})
+// app.delete('/courses', (req, res) => {
+//     const id = req.body.id;
+//     for (let j=0; j<courses.length; j++){
+//         if (courses[j].id === id){
+//             courses.splice(j, 1);
+//         }
+//     }
+//     res.send(courses);
+// })
 
 // Creating Custom Middlewear
-function middleware(req, res, next){
-    console.log("called");
-    next();
-}
+// function middleware(req, res, next){
+//     console.log("called");
+//     next();
+// }
 // logger middleware
 // method, ip, hostname, date
-function requestLogger(req, res, next){
-    console.log(req.ip);
-    console.log(req.hostname);
-    console.log(req.method);
-    console.log(new Date().toISOString());
-    next();
-}
-const host = 'localhost';
-const port = 3000;
-
-app.listen(port, () => {
-    console.log('Server has started.');
-})
-
+// function requestLogger(req, res, next){
+//     console.log(req.ip);
+//     console.log(req.hostname);
+//     console.log(req.method);
+//     console.log(new Date().toISOString());
+//     next();
+// }
 // Product Schema
 const productSchema = new mongoose.Schema({
     product_name : {
@@ -92,8 +85,39 @@ const productSchema = new mongoose.Schema({
         type : Boolean,
         required : true
     },
-    catgegory : {
+    category : {
         type : String,
         required : true
     }
 });
+const productModel = mongoose.model("products", productSchema);
+// Create a product
+app.post('/api/products', async (req, res) =>{
+    const body = req.body;
+    const product = await productModel.create({
+        product_name : req.body.product_name,
+        product_price : req.body.product_price,
+        isInStock : req.body.isInStock,
+        category : req.body.category
+    })
+    console.log(product);
+    return res.status(201).json({message : 'Product Created Successfully.'});
+})
+
+app.get('/api/products', async (req, res) => {
+    const allProducts = await productModel.find({});
+    return res.status(200).json({message: 'Products fetched successfully', allProducts});
+})
+
+// Update Product Details
+app.put('/api/products/:id', async (req, res) => {
+    const updatedProduct = await productModel.findByIdAndUpdate(req.params.id, req.body);
+    return res.json(updatedProduct);
+})
+const host = 'localhost';
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server has started.');
+
+})
